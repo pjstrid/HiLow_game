31c3838abea7d0dadf0ffc43c7f04b6a82822eb4
jonathan strid <pjstrid@outlook.com>
2025-09-22 10:21:37 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,131 @@
+import java.util.Random;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    private static final Scanner in = new Scanner(System.in);
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+
+        // HiLo - Guessing game
+
+
+         runHiLo(random);
+
+    }
+
+    private static void runHiLo(Random random) {
+        System.out.println("Välkommen till HiLo");
+        System.out.println("Vilken nivå vill du köra?");
+        System.out.println();
+        String easy = "Lätt";
+        String medium = "Mellan";
+        String hard = "Svår";
+        System.out.println(1+": "+easy);
+        System.out.println(2+": "+medium);
+        System.out.println(3+": "+hard);
+        System.out.println();
+        System.out.println("Ditt val:");
+
+        String level = in.nextLine().toLowerCase();
+
+
+        if (level.equalsIgnoreCase(easy) || level.equals("1")) {
+            levelEasy(random);
+        } else if (level.equalsIgnoreCase(medium) || level.equals("2")) {
+            levelMedium(random);
+        } else if (level.equalsIgnoreCase(hard) || level.equals("3")){
+            levelHard(random);
+        } else {
+            System.out.println("Ogiltigt val. Välj igen");
+            runHiLo(random);
+        }
+
+    }
+
+
+
+    private static void levelEasy(Random random) {
+        int computerChoice = random.nextInt(1, 11);
+        System.out.println("--------------------");
+        System.out.println("Du valde nivå: Lätt");
+        System.out.println("Gissa mitt hemliga tal.. 1-10");
+
+        int guessCount = 0;
+        boolean playing = true;
+
+        while (playing) {
+            int playerGuess = in.nextInt();
+
+            if (playerGuess < computerChoice) {
+                System.out.println("Högre");
+                guessCount++;
+            } else if (playerGuess > computerChoice) {
+                System.out.println("Lägre");
+                guessCount++;
+            } else {
+                playing = false;
+                guessCount++;
+                System.out.println("Rätt!");
+                System.out.println("Antal gissningar: "+guessCount);
+            }
+        }
+    }
+
+    private static void levelMedium(Random random) {
+        int computerChoice = random.nextInt(1, 101);
+        System.out.println("--------------------");
+        System.out.println("Du valde nivå: Mellan");
+        System.out.println("Gissa mitt hemliga tal.. 1-100");
+
+        int guessCount = 0;
+        boolean playing = true;
+
+        while (playing) {
+            int playerGuess = in.nextInt();
+
+            if (playerGuess < computerChoice) {
+                System.out.println("Högre");
+                guessCount++;
+            } else if (playerGuess > computerChoice) {
+                System.out.println("Lägre");
+                guessCount++;
+            } else {
+                playing = false;
+                guessCount++;
+                System.out.println("Rätt!");
+                System.out.println("Antal gissningar: "+guessCount);
+            }
+        }
+    }
+
+    private static void levelHard(Random random) {
+        int computerChoice = random.nextInt(1, 1001);
+        System.out.println("--------------------");
+        System.out.println("Du valde nivå: Svår");
+        System.out.println("Gissa mitt hemliga tal.. 1-1000");
+
+        int guessCount = 0;
+        boolean playing = true;
+
+        while (playing) {
+            int playerGuess = in.nextInt();
+
+            if (playerGuess < computerChoice) {
+                System.out.println("Högre");
+                guessCount++;
+            } else if (playerGuess > computerChoice) {
+                System.out.println("Lägre");
+                guessCount++;
+            } else {
+                playing = false;
+                guessCount++;
+                System.out.println("Rätt!");
+                System.out.println("Antal gissningar: "+guessCount);
+            }
+        }
+    }
+}
