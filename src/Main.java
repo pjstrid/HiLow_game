11e256795c4bfb8fc6347e1f00f6491f07f08654
jonathan strid <pjstrid@outlook.com>
2025-09-22 10:42:44 +0100

    added play again menu

--- src/Main.java
+++ src/Main.java
@@ -18,31 +18,54 @@ public class Main {
     }
 
     private static void runHiLo(Random random) {
-        System.out.println("Välkommen till HiLo");
-        System.out.println("Vilken nivå vill du köra?");
-        System.out.println();
-        String easy = "Lätt";
-        String medium = "Mellan";
-        String hard = "Svår";
-        System.out.println(1+": "+easy);
-        System.out.println(2+": "+medium);
-        System.out.println(3+": "+hard);
-        System.out.println();
-        System.out.println("Ditt val:");
 
-        String level = in.nextLine().toLowerCase();
+            System.out.println();
+            System.out.println("Välkommen till HiLo");
+            System.out.println("Vilken nivå vill du köra?");
+            System.out.println();
+            String easy = "Lätt";
+            String medium = "Mellan";
+            String hard = "Svår";
+            System.out.println(1 + ": " + easy);
+            System.out.println(2 + ": " + medium);
+            System.out.println(3 + ": " + hard);
+            System.out.println();
+            System.out.println("Ditt val:");
+
+            String level = in.nextLine().toLowerCase();
 
 
-        if (level.equalsIgnoreCase(easy) || level.equals("1")) {
-            levelEasy(random);
-        } else if (level.equalsIgnoreCase(medium) || level.equals("2")) {
-            levelMedium(random);
-        } else if (level.equalsIgnoreCase(hard) || level.equals("3")){
-            levelHard(random);
-        } else {
-            System.out.println("Ogiltigt val. Välj igen");
-            runHiLo(random);
-        }
+            if (level.equalsIgnoreCase(easy) || level.equals("1")) {
+                levelEasy(random);
+            } else if (level.equalsIgnoreCase(medium) || level.equals("2")) {
+                levelMedium(random);
+            } else if (level.equalsIgnoreCase(hard) || level.equals("3")) {
+                levelHard(random);
+            } else {
+                System.out.println("Ogiltigt val. Välj igen");
+                runHiLo(random);
+            }
+
+            menu();
+    }
+
+    private static void menu() {
+            System.out.println("_______________________");
+            System.out.println("Vill du spela igen?");
+            System.out.println("1. Ja");
+            System.out.println("2. Nej");
+            int choice = in.nextInt();
+            in.nextLine();
+
+            switch(choice) {
+                case 1:
+                    runHiLo(random);
+                    break;
+                case 2:
+                    break;
+                default:
+            }
+
 
     }
 
